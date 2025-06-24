cf1ee97b00755de139be73262471600e5de166b3
vits03 <101546379+vits03@users.noreply.github.com>
2025-06-24 22:02:47 +0400

    added resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="/resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
